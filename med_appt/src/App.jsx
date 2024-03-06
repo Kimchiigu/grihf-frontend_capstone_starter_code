85badcfcc66b5b022f2a896318af68b6781b27eb
Kimchiigu <christopher.hygunawan@gmail.com>
2024-03-06 12:49:04 +0700

    Finalized

--- med_appt/src/App.jsx
+++ med_appt/src/App.jsx
@@ -1,38 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-
-import Login from "./Components/Login/Login"
-import SignUp from "./Components/Sign_Up/Sign_Up"
-import Navbar from "./Components/Navbar/Navbar"
-import Layout from "./Components/Landing_Page/Landing_Page"
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ReviewForm from "./Components/ReviewForm/ReviewForm"
+import Layout from "./Components/Landing_Page/Landing_Page";
+import Login from "./Components/Login/Login";
+import SignUp from "./Components/Sign_Up/Sign_Up";
+import ProfileCard from "./Components/ProfileCard/ProfileCard"
+import ReportsLayout from "./Components/ReportsLayout/ReportsLayout"
+import InstantConsultation from './Components/InstantConsultation/InstantConsultation';
+import BookingConsultation from './Components/BookingConsultation/BookingConsultation';
+import Healthblog from './Components/Healthblog/Healthblog';
+import SelfCheckup from './Components/SelfCheckup/SelfCheckup';
+import Home from './Components/Home/Home';
+import Navbar from './Components/Navbar/Navbar';
+import Notification from './Components/Notification/Notification';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className='App'>
+    <div className="App">
       <BrowserRouter>
-            <Navbar/>
-                <Routes>
-                <Route path="/" element={<Layout/>}/>
-                {/* <Route path="/Home" element={<Home/>} /> */}
-                <Route path="/Login" element={<Login/>}/>
-                <Route path="/SignUp" element={<SignUp/>}/>
-                {/* <Route path="/InstantConsultation" element={<InstantConsultation/>} />
-                <Route path="/BookingConsultation" element={<BookingConsultation/>} />
-                <Route path="/Healthblog" element={<Healthblog/>} />
-                <Route path="/SelfCheckup" element={<SelfCheckup/>} />
-                <Route path="/ReviewForm" element={<ReviewForm/>}/>
-                <Route path="/ProfileCard" element={<ProfileCard/>}/>
-                <Route path="/ReportsLayout" element={<ReportsLayout/>}/> */}
-                </Routes>
-           {/* <Notification/> */}
-          </BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Layout />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/InstantConsultation" element={<InstantConsultation />} />
+          <Route path="/BookingConsultation" element={<BookingConsultation />} />
+          <Route path="/Healthblog" element={<Healthblog />} />
+          <Route path="/SelfCheckup" element={<SelfCheckup />} />
+          <Route path="/ReviewForm" element={<ReviewForm />} />
+          <Route path="/ProfileCard" element={<ProfileCard />} />
+          <Route path="/ReportsLayout" element={<ReportsLayout />} />
+        </Routes>
+        <Notification />
+      </BrowserRouter>
+
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
